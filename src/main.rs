0e13a47b52c85ae7723ef74b95ae34aa9b222bec
NicoleKai <nicolekohm102@gmail.com>
2023-10-05 02:06:49 -0700

    simplified

--- src/main.rs
+++ src/main.rs
@@ -331,45 +331,44 @@ fn mat4_ui<'a>(ui: &mut Ui, ui_state: &mut UiState, value: &mut Mat4) {
             ui.end_row();
 
             ui.colored_label(egui::Color32::from_rgb(128, 128, 64), "X");
-            ui.matrix_drag(0, s, &mut value.x_axis.x, 1.0, "Mat4: x_axis, Vec4: x");
-            ui.matrix_drag(1, s, &mut value.x_axis.y, 0., "Mat4: x_axis, Vec4: y");
-            ui.matrix_drag(2, s, &mut value.x_axis.z, 0., "Mat4: x_axis, Vec4: z");
-            ui.matrix_drag(3, s, &mut value.w_axis.x, 0., "Mat4: w_axis, Vec4: x");
+            ui.matrix_drag(0, s, &mut value.x_axis.x, 1.0, "x_axis.x");
+            ui.matrix_drag(1, s, &mut value.x_axis.y, 0., "x_axis.y");
+            ui.matrix_drag(2, s, &mut value.x_axis.z, 0., "x_axis.z");
+            ui.matrix_drag(3, s, &mut value.w_axis.x, 0., "w_axis.x");
             ui.end_row();
 
             ui.colored_label(egui::Color32::from_rgb(128, 128, 64), "Y");
-            ui.matrix_drag(4, s, &mut value.y_axis.x, 0., "Mat4: y_axis, Vec4: x");
-            ui.matrix_drag(5, s, &mut value.y_axis.y, 1.0, "Mat4: y_axis, Vec4: y");
-            ui.matrix_drag(6, s, &mut value.y_axis.z, 0., "Mat4: y_axis, Vec4: z");
-            ui.matrix_drag(7, s, &mut value.w_axis.y, 0., "Mat4: w_axis, Vec4: y");
+            ui.matrix_drag(4, s, &mut value.y_axis.x, 0., "y_axis.x");
+            ui.matrix_drag(5, s, &mut value.y_axis.y, 1.0, "y_axis.y");
+            ui.matrix_drag(6, s, &mut value.y_axis.z, 0., "y_axis.z");
+            ui.matrix_drag(7, s, &mut value.w_axis.y, 0., "w_axis.y");
             ui.end_row();
 
             ui.colored_label(egui::Color32::from_rgb(128, 128, 64), "Z");
-            ui.matrix_drag(8, s, &mut value.z_axis.x, 0., "Mat4: z_axis, Vec4: x");
-            ui.matrix_drag(9, s, &mut value.z_axis.y, 0., "Mat4: z_axis, Vec4: y");
-            ui.matrix_drag(10, s, &mut value.z_axis.z, 1.0, "Mat4: z_axis, Vec4: z");
-            ui.matrix_drag(11, s, &mut value.w_axis.z, 0., "Mat4: w_axis, Vec4: z");
+            ui.matrix_drag(8, s, &mut value.z_axis.x, 0., "z_axis.x");
+            ui.matrix_drag(9, s, &mut value.z_axis.y, 0., "z_axis.y");
+            ui.matrix_drag(10, s, &mut value.z_axis.z, 1.0, "z_axis.z");
+            ui.matrix_drag(11, s, &mut value.w_axis.z, 0., "w_axis.z");
             ui.end_row();
 
             ui.colored_label(egui::Color32::from_rgb(128, 128, 64), "W");
-            ui.matrix_drag(12, s, &mut value.x_axis.w, 0., "Mat4: x_axis, Vec4: w");
-            ui.matrix_drag(13, s, &mut value.y_axis.w, 0., "Mat4: y_axis, Vec4: w");
-            ui.matrix_drag(14, s, &mut value.z_axis.w, 0., "Mat4: z_axis, Vec4: w");
-            ui.matrix_drag(15, s, &mut value.w_axis.w, 1.0, "Mat4: w_axis, Vec4: w");
+            ui.matrix_drag(12, s, &mut value.x_axis.w, 0., "x_axis.w");
+            ui.matrix_drag(13, s, &mut value.y_axis.w, 0., "y_axis.w");
+            ui.matrix_drag(14, s, &mut value.z_axis.w, 0., "z_axis.w");
+            ui.matrix_drag(15, s, &mut value.w_axis.w, 1.0, "w_axis.w");
             ui.end_row();
         });
-        ui.separator();
-        ui.horizontal(|ui| {
-            if ui.button("Reset All").clicked() {
-                *s = CtrlsState::default();
-            }
-            if ui.button("Reset Matrix Values").clicked() {
-                s.reset_values();
-            }
-            if ui.button("Reset Mode Selections").clicked() {
-                s.reset_modes();
-            }
-        });
+    });
+    ui.horizontal(|ui| {
+        if ui.button("Reset All").clicked() {
+            *s = CtrlsState::default();
+        }
+        if ui.button("Reset Matrix").clicked() {
+            s.reset_values();
+        }
+        if ui.button("Reset Mode").clicked() {
+            s.reset_modes();
+        }
     });
     ui.separator();
     ui.strong("High-level Controls");
